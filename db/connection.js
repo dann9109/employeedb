0fdf963b462565a39eb88878569d07056fceee50
dann9109 <danielxstella@gmail.com>
2024-01-28 16:01:46 -0500

    seedconnection

--- db/connection.js
+++ db/connection.js
@@ -3,6 +3,8 @@ const mysql = require('mysql2/promise');
 const fs = require('fs');
 
 const sqlFile = fs.readFileSync('./db/schema.sql', 'utf8');
+const seedFile = fs.readFileSync('./db/seed.sql', 'utf8');
+const queryFile = fs.readFileSync('./db/query.sql', 'utf8');
 const sqlStatements = sqlFile.split(';').map((statement) => statement.trim());
 
 // Create a connection pool to the MySQL database
